d0a3035679033f71a055efb097bd3b6564ee6b7c
jinhyeon <vmflwu123@gmail.com>
2024-02-23 12:57:57 +0900

    포트폴리오 생성 api 추가

--- routes/portfolio/portfolios.js
+++ routes/portfolio/portfolios.js
@@ -1,26 +1,75 @@
 const express = require('express');
 const router = express.Router();
-const StockItem = require('../../model/StockItem');
+const Portfolio = require('../../model/Portfolio');
 const {getInvestResult} = require('../../sevices/portfolio/PortfolioService');
+const {authenticate} = require('../user');
+const CreateInvestResultDto = require('../../dto/request/CreateInvestResultDto');
+const CreatePortfolioDto = require('../../dto/request/CreatePortfolioDto');
+
+router.use(authenticate, function(req, res, next) {
+    next();
+});
 
 /**
  * 포트폴리오 구성 및 수익률 계산
  * POST /portfolios
+ * <request>
  * {
+ *     "name": "My Portfolio",
+ *     "duration": {
+ *         "startDate": "2022-01-01",
+ *         "endDate": "2022-12-31"
+ *     },
+ *     "investAmount": 1000000,
+ *     "itemCodes": ["000080", "000100", "000120"],
+ *     "weights": [0.3, 0.3, 0.4]
+ * }
+ * <response>
+ * {
+ *     _id: "65d80657b4b09c5c00e690b4",
+ *     name: "My Portfolio",
+ *     stockItems: [
+ *         {
+ *             item: "65d4857362d0c49dc9cd20d9",
+ *             weight: {
+ *                 "$numberDecimal": "0.3",
+ *             },
+ *             _id: ~
+ *         },
+ *         ...
+ *     ],
+ *     totalReturnRates: [
+ *         {
+ *             date: "2022-01-01",
+ *             rate: {
+ *             "$numberDecimal": "0.0",
+ *             },
+ *         },
+ *         ...
+ *     ],
+ *     evaluationAmount: {
+ *         "$numberDecimal": "1000000",
+ *     }
  *     duration: {
  *         startDate: "2022-01-01",
  *         endDate: "2022-12-31"
  *     },
- *     investAmount: 1000000,
- *     itemCodes: ["000080", "000100", "000120"],
- *     weights: [0.3, 0.3, 0.4]
+ *     createdAt: "2022-01-01T00:00:00.000Z",
+ *     updatedAt: "2022-01-01T00:00:00.000Z",
  * }
  */
 router.post('/', async function(req, res, next) {
-    getInvestResult()
-        .then(result => {
-            res.json(result);
-        }).catch(err => {
-        console.error(err);
-    })
+    try {
+        const createInvestResultDto = CreateInvestResultDto.fromRequest(req);
+        const result = await getInvestResult(createInvestResultDto);
+
+        const createPortfolioDto = CreatePortfolioDto.fromRequest(req, result);
+        const portfolio = await Portfolio.create(createPortfolioDto);
+        res.json(portfolio);
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
 });
+
+module.exports = router;
