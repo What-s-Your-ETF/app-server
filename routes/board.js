e82100fc8b29a13aa3e86ed072903d4c0617636d
Tomk2d <uijinshin01@gmail.com>
2024-02-26 16:13:36 +0900

    update board

---
+++ routes/board.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+let Board = require('../model/Board.js');
+
+router.get('/', (req, res, next)=>{
+    Board.find().then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        return next(err)
+    })
+});
+
+router.get('/:id',(req,res,next)=>{
+    
+    Board.findById(req.params.id).then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        return next(err);
+    })
+});
+
+router.post('/',(req,res,next)=>{
+    
+    Board.create(req.body).then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        return next(err);
+    })
+});
+
+router.put('/:id',(req,res,next)=>{
+    Board.findByIdAndUpdate(req.params.id,{ title:req.body.title, content: req.body.content, author:req.body.author})
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        return next(err);
+    })
+});
+
+router.delete('/:id',(req,res,next)=>{
+    Board.findByIdAndDelete(req.params.id).then(data=>{
+        res.json(data);
+    })
+    .catch(err=>{
+        return next(err);
+    })
+});
+
+
+module.exports = router;
