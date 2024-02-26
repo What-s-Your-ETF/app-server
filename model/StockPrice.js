fbdcabe5027ba9c109b42d1575f42586f4d93df3
jinhyeon <vmflwu123@gmail.com>
2024-02-26 14:12:45 +0900

    StockPrice 스키마 수정

--- model/StockPrice.js
+++ model/StockPrice.js
@@ -8,26 +8,21 @@ const stockPriceSchema = new mongoose.Schema({
         },
         startPrice: { // 시가
             type: Number,
-            required: true
         },
         endPrice: { // 종가
             type: Number,
-            required: true
         },
         highPrice: { // 고가
             type: Number,
-            required: true
         },
         lowPrice: { // 저가
             type: Number,
-            required: true
         },
         volume: { // 거래량
             type: mongoose.Types.Decimal128,
         },
         volatility: { // 변동률
             type: mongoose.Types.Decimal128,
-            required: true
         },
         date: { // 날짜
             type: Date,
